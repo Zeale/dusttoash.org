23288b2ceb7437c20d68f2ce83b1e69a937b8cf6
Zeale <Zeale@dusttoash.org>
2018-05-22 16:35:14 -0500

    Website now has a working header

--- index.php
+++ index.php
@@ -4,17 +4,23 @@
     <title>Dust To Ash</title>
     <link href="index.css" rel="stylesheet" type="text/css">
     <script src="index.js" type="text/javascript"></script>
+
+    <!-- Acme font -->
+    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
 </head>
 
 <body class="background">
     <div class="header full-width" id="Header">
         <!-- This is the navigation header -->
 
-        <div class="spacing" style="height: 30px;"></div>
-        <div class="header-content" style="height: 50px;"></div>
+        <span>Test</span>
+        <span>Test</span>
+        <span>Test</span>
+        <span>Test</span>
+        <span>Test</span>
     </div>
 
-    <div class="content-root">Hello</div>
+    <div class="content-root"></div>
 </body>
 
 </html>
