2354937919f00925b99defc384ab86954e68667d
Zeale <Zeale@dusttoash.org>
2018-05-21 20:26:34 -0500

    Basic homepage modification

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Hi!
+<html lang="en">
+    <head>
+        <title>Dust To Ash</title>
+        <link href="index.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="header special-style-div"></div>
+        <div>Hello</div>
+    </body>
+</html>
