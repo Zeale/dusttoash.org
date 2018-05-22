bcdd6640750ef86552354be7275eb5f8c8341b82
Zeale <Zeale@dusttoash.org>
2018-05-22 09:39:12 -0500

    Updated homepage

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <html lang="en">
-    <head>
-        <title>Dust To Ash</title>
-        <link href="index.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <div class="header special-style-div"></div>
-        <div>Hello</div>
-    </body>
+
+<head>
+    <title>Dust To Ash</title>
+    <link href="index.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="background">
+    <div class="header full-width">
+        <!-- This is the navigation header -->
+
+        <div class="spacing" style="height: 30px;"></div>
+        <div class="header-content" style="height: 50px;"></div>
+    </div>
+
+    <div class="content-root">Hello</div>
+</body>
+
 </html>
