cddd921c9d5064a01cadaae1f0b8c7b7286ca4db
Zeale <Zeale@dusttoash.org>
2018-05-23 22:45:20 -0500

    Finished styling and creating anchors

--- index.php
+++ index.php
@@ -18,7 +18,8 @@
     </div>
 
     <div class="content-root">
-        <h1 class="heading">Content Text<a id="Content Text" class="anchor">&lt;a&gt;</a></h1>
+        <!-- Header tags IDs that are referred to by Anchors should be formatted either like "ContentText" below (where each word is capitalized with no spacing) or like "content-text" (where each word is lowercase and separated by a single hyphen). This is the general format/mini spec that I'll be using for anchor tags.  -->
+        <h1 class="heading" id="ContentText">Content Text<a href="#ContentText" class="anchor">&lt;a&gt;</a></h1>
         Some regular content text; this text appears a deep, sky blue, while headers, like <span style="color: var(--secondary-color);">Message</span>, appear a <span style="color: var(--secondary-color);">coral-like color</span>.
     </div>
 
