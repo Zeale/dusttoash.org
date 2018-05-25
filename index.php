7122b9b01049f876fad703a8e64cfbf775118cf3
Zeale <Zeale@dusttoash.org>
2018-05-24 20:27:01 -0500

    Made "centered" span elements actually become centered

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "autoload"; ?>
 <?php t(); ?>
 
-<?php heading("Content Text", "ContentText"); ?>
-Some regular content text; this text appears a deep, sky blue, while headers, like <span style="color: var(--secondary-color);">Message</span>, appear a <span style="color: var(--secondary-color);">coral-like color</span>.
+<?php heading("Welcome"); ?>
+<span class="centered">This page has not yet been filled out.</span>
 
 <?php b(); ?>
