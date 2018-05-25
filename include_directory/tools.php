a5f7b3c0c247123079450a4f8fcecd80758011a5
Zeale <Zeale@dusttoash.org>
2018-05-24 20:13:51 -0500

    Updated the heading PHP tool

--- include_directory/tools.php
+++ include_directory/tools.php
@@ -1,8 +1,8 @@
 <?php function anchor($target) { ?>
     <a href="#<?php echo $target;?>" class="anchor">&lt;a&gt;</a>
 <?php } ?>
-<?php function heading($text, $id) { 
-    if(!isset($id))
+<?php function heading($text, $id=null) { 
+    if($id==null)
         $id=$text;
 ?>
     <h1 class="heading" id="<?php echo $id;?>"><?php echo $text; anchor($id);?></h1>
