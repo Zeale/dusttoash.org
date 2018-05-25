5f8e8a60f4235c84c0038b4cfd62e057a2936cc3
Zeale <Zeale@dusttoash.org>
2018-05-24 20:51:31 -0500

    Added some crap to the about page

---
+++ about/index.php
@@ -0,0 +1,15 @@
+<?php include "autoload"; ?>
+<?php t(); ?>
+
+<?php heading("About"); ?>
+This website is, <em style="color: var(--secondary-color);">(currently)</em>, basically a styled, human readable repository for programs.<br><br>
+I'll add a dynamic list of programs, or something, that you can check back on frequently or whatever. From there you will be able to download and mess with whatever content you want.<br><br>
+Soon, once accounts are added, you'll be able to store data from certain programs from this site, on you account at this site, as well as do other things here.<br><br>
+
+<?php heading("Programs") ?>
+A lot of the content that I make gets uploaded here. There <em style="color: var(--secondary-color);">might</em> things like a Chat Room, Calculator, and some other random crap. I may also upload some content having to do with code in general, or maybe some tutorials or something (on programming). Some good documentation will be nice.<br><br>
+
+<?php heading("Other Stuff", "OtherStuff"); ?>
+I may upload some files (stowed away in some folder) on some random things. I might hide them from being listed out by any accessing crawlers, or even people, if I get around to it.<br><br>
+
+<?php b(); ?>
