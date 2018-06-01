a03e154616e92f6ac08636d46ae9c2cb1ad04324
Zeale <Zeale@dusttoash.org>
2018-06-01 01:36:02 -0500

    I listen to this while working on this proj...

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 <?php heading("Welcome"); ?>
 <div>This page has not yet been filled out.</div>
+<div>In the mean time, why don't you listen to this on repeat for a few hours:<br><br></div>
+<iframe width="300px" height="300px" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/240088594&color=%2300bfff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
 <span class="pop-text">Here is some popping text.</span>
 
 <?php b(); ?>
