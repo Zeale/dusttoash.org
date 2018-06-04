12451030f18cbfd235f397517c923fbaa75a6c68
Zeale <Zeale@dusttoash.org>
2018-06-03 20:58:50 -0500

    Page header items now reflect being logged in

--- include_directory/templates/top.php
+++ include_directory/templates/top.php
@@ -1,51 +1,49 @@
 <?php
-
+use dusttoash\logins\Database;
 class HeaderItem {
-    
-    private $name, $link;
-    
-    public function print() {
-        echo "<span onclick=\"document.location.href='" . $this->link . "'\">" . $this->name . "</span>";
-    }
-    
-    function __construct(string $name, string $link){
-        $this->name=$name;
-        $this->link=$link;
-    }
-
+	private $name, $link;
+	public function print() {
+		echo "<span onclick=\"document.location.href='" . $this->link . "'\">" . $this->name . "</span>";
+	}
+	function __construct(string $name, string $link) {
+		$this->name = $name;
+		$this->link = $link;
+	}
 }
 
 // I'll deal with namespaces and stuff later.
-$defaultHeaderItems = array(new HeaderItem("Home","/"), new HeaderItem("Login","/login"),new HeaderItem("About","/about"));
 
 ?>
 <?php function t(bool $centerContent=null, bool $includeDefaults=null, HeaderItem ...$headings) { ?>
 <html lang="en">
 
 <head>
-    <title>Dust To Ash</title>
-    <link href="/index.css" rel="stylesheet" type="text/css">
-    <script src="/index.js" type="text/javascript"></script>
+<title>Dust To Ash</title>
+<link href="/index.css" rel="stylesheet" type="text/css">
+<script src="/index.js" type="text/javascript"></script>
 
-    <!-- Acme font -->
-    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+<!-- Acme font -->
+<link href="https://fonts.googleapis.com/css?family=Acme"
+	rel="stylesheet">
 </head>
 
 <body class="background">
-    <div class="header full-width" id="Header">
-        <!-- This is the navigation header -->
-<?php if(count($headings)==0 || $includeDefaults):?>
-        <span onclick="document.location.href='/'">Home</span>
-        <span onclick="document.location.href='/login'">Login</span>
-        <span onclick="document.location.href='/about'">About</span>
-        <?php 
-else:
-    foreach($headings as $span) {
-        $span->print() . "\n";
-    }
-endif;
-?>
+	<div class="header full-width" id="Header">
+		<!-- This is the navigation header -->
+<?php
+	
+	if (count ( $headings ) == 0 || $includeDefaults) {
+		array_push ( $headings, new HeaderItem ( "Home", "/" ), new HeaderItem ( "About", "/about" ) );
+		if ((new Database ())->isLoggedIn ())
+			array_push ( $headings, new HeaderItem ( "Sign Out", "/sign-out" ) );
+		else
+			array_push ( $headings, new HeaderItem ( "Login", "/login" ), new HeaderItem ( "Sign Up", "/sign-up" ) );
+	}
+	foreach ( $headings as $span ) {
+		$span->print () . "\n";
+	}
+	?>
     </div>
 
-    <div class="content-root<?php if($centerContent)echo " centered"; ?>">
+	<div class="content-root<?php if($centerContent)echo " centered"; ?>">
         <?php } ?>
