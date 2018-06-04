44dc10057d06de34ad24b24714a60beced86b853
Zeale <Zeale@dusttoash.org>
2018-06-03 22:29:33 -0500

    Added some stuff to the login page

--- login/index.php
+++ login/index.php
@@ -1,6 +1,32 @@
-<?php t(); ?> 
-<?php heading("Login", "Login"); ?>
+<?php
+use dusttoash\logins\Database;
+$loggedIn = (new Database ())->isLoggedIn ();
+function printForm(string ...$errors) {
+	global $loggedIn;
+	
+	t ();
+	
+	if ($loggedIn)
+		echo "<span style='color: var(--hard-gold);'>You are already logged in. Logging to a new account will log you out of your current account.</span>";
+	
+	// Print the errors from the previous attempt
+	foreach ( $errors as $error )
+		echo "<span style='color: var(--hard-red);'>$error</span>";
+	?>
+<style>
+form {
+	color: var(--secondary-color);
+}
+</style>
+<form style="margin-top: 50px;" method="post">
+	Username/Email: <input type="text" name="identifier"
+		<?php if(isset($identifier)) echo " value='" . $identifier. "'";?>> <br>
+	<br> <br> Password: <input type="password" name="password"
+		<?php if(isset($password)) echo " value='" . $password . "'";?>> <br>
+	<br> <br> <input type="submit">
+</form>
+<?php
+}
 
-<span class="centered">Please don't enter in any information that you keep private. This site is under development and we don't want your information to get stolen.</span>
-
-<?php b(); ?>
+if (isset ( $_POST ['identifier'] ) && isset ( $_POST ['password'] )) {
+}
