1b18ef07d66e78f35f8f9a869c8cc80f568199ad
Zeale <Zeale@dusttoash.org>
2018-06-11 02:57:48 -0500

    Removed most duplicate code in meme set pages

--- memes/index.php
+++ memes/index.php
@@ -4,6 +4,6 @@ use templates\Printer;
 Printer::getDefaultPrinter ()->addHeadIncludes ( "<link href=\"index.css\" rel=\"stylesheet\" type=\"text/css\">" );
 t ();
 
-for($i = 1;$i < sizeof(glob ( "set-*/")) + 1; $i++)
-    echo "<div><a class='meme-set-link' href='set-" . $i . "'>Set-" . $i . "</a></div>";
+for($i = 1;$i < sizeof(glob ( "sets/set-*/")) + 1; $i++)
+    echo "<div><a class='meme-set-link' href='sets/set-" . $i . "'>Set-" . $i . "</a></div>";
 b ();
