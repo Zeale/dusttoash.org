053f6894c07a6ab018612cdd276fad4c5df65c85
Zeale <Zeale@dusttoash.org>
2018-06-11 02:41:35 -0500

    Updated homepage

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php t(true); ?>
 
 <?php heading("Welcome"); ?>
-<div>This page has not yet been filled out.</div>
-<div>In the mean time, why don't you listen to this on repeat for a few hours:<br><br></div>
-<iframe width="300px" height="300px" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/240088594&color=%2300bfff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-<span class="pop-text">Here is some popping text.</span>
-
+<div>This site is under construction.</div>
+<h4><i>However...</i></h4>
+<div>It does have a few features that you can browse through. For starters,<br><br></div>
+<?php heading("Memes"); ?>
+<div><a href="/memes/">Here</a> is the meme page. Check out different sets of <span style="color: var(--hard-red);">highly offensive</span> memes here. New sets are added whenever I get good memes.</div>
+<?php heading("Programs"); ?>
+<div>You can find a list of programs that I've developed (and, probably download most of them) at the <a href="/applications/">applications</a> page.</div>
 <?php b(); ?>
