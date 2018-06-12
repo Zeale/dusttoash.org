eb975731b3a9af44e5aaf0ad831befc82392d50d
Zeale <Zeale@dusttoash.org>
2018-06-12 17:23:15 -0500

    The font tag
    Replaced a span element with a font element to make it not break lines (because spans' displays are set to break lines in my CSS I think).

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h4><i>However...</i></h4>
 <div>It does have a few features that you can browse through. For starters,<br><br></div>
 <?php heading("Memes"); ?>
-<div><a href="/memes/">Here</a> is the meme page. Check out different sets of <span style="color: var(--hard-red);">highly offensive</span> memes here. New sets are added whenever I get good memes.</div>
+<div><a href="/memes/">Here</a> is the meme page. Check out different sets of <font style="color: var(--hard-red);">highly offensive</font> memes here. New sets are added whenever I get good memes.</div>
 <?php heading("Programs"); ?>
 <div>You can find a list of programs that I've developed (and, probably download most of them) at the <a href="/applications/">applications</a> page.</div>
 <?php b(); ?>
