6635dfc1fe77beb157f464c0822654430baedd58
Zeale <Zeale@dusttoash.org>
2018-09-01 22:04:15 -0500

    Added console things and stuff.

---
+++ console/index.php
@@ -0,0 +1,5 @@
+<?php
+use templates\HeaderItem;
+t(true, null, array("console"), new HeaderItem("Home", "/")); ?>
+
+<?php b(); ?>
--- testing/index.php
+++ testing/index.php
@@ -1,3 +1,6 @@
-<?php
-t();
-b();
+<?php t(true, null, array("console")); ?>
+<?php heading("Code Blocks"); ?>
+<div>A call to the
+    <pre class="inline-code">printCode(resource)</pre>, given the CSS file that styles the code, produces the following:</div>
+<?php printCode(fopen("C:\wamp64\dusttoash.org\stylesheets\console.css", "r")); ?>
+<?php b(); ?>
