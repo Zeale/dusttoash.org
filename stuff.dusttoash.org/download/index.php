ac953932ed52ca95b595f8383aba9a88698793c8
Zeale <Zeale@dusttoash.org>
2019-05-06 01:29:33 -0500

    Made the update page read in values from file

--- stuff.dusttoash.org/download/index.php
+++ stuff.dusttoash.org/download/index.php
@@ -16,10 +16,10 @@
         <p>Quick note before you download: This app is a work in progress and will be updated frequently. Hence, it may contain small bugs. If you'd like to receive any updates, you'll need to check back at this page for newer versions periodically, until the application is capable of checking for updates on its own (hopefully that will be soon!).</p>
         <p>Please click <a class=text-link href="/license">here</a> for the program's license, before downloading or installing the application. By using the application you agree to this license.</p>
         <br>
-        <em><p>Stuff's current version is <u>0.1.2</u> and was released on <u>5/2/2019</u>.</p></em>
+        <em><p>The latest version, (<u><?php $u=false;$f=fopen("latest.txt", "r")or$u=true;echo$v=($u?"Err reading ver":trim(fgets($f)));?></u>), was released on <u><?php echo $u?"Err reading ver":trim(fgets($f));?></u> and is <?php echo $u?"Err reading ver":trim(fgets($f));?>.</p></em>
         <br>
-        <a href="Stuff%20Installer.exe" target=_blank><div class=button">Windows Installer (v0.1.2)</div></a>
-        <a href="Stuff.jar" target=_blank><div class=button">Cross Platform Executable (v0.1.2)</div></a>
+        <a href="Stuff%20Installer.exe" target=_blank><div class=button">Windows Installer (<?php echo$v;?>)</div></a>
+        <a href="Stuff.jar" target=_blank><div class=button">Cross Platform Executable (<?php echo$v;?>)</div></a>
       </span>
     </div>
 	</body>
