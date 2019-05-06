b87c701c16eb6fad0d601d7c8e8e9e8bac00b17a
Zeale <Zeale@dusttoash.org>
2019-05-06 03:15:51 -0500

    Added changelog link to download page

--- stuff.dusttoash.org/download/index.php
+++ stuff.dusttoash.org/download/index.php
@@ -3,6 +3,7 @@
 <br>
 <p>Quick note before you download: This app is a work in progress and will be updated frequently. Hence, it may contain small bugs. If you'd like to receive any updates, you'll need to check back at this page for newer versions periodically, until the application is capable of checking for updates on its own (hopefully that will be soon!).</p>
 <p>Please click <a class=text-link href="/license">here</a> for the program's license, before downloading or installing the application. By using the application you agree to this license.</p>
+<p>You can find a changelog for the current version <b><a class=text-link href="changelog">here</a></b>.</p>
 <br>
 <em><p>The latest version, (<u><?php $u=false;$f=fopen("latest.txt", "r")or$u=true;echo$v=($u?"Err reading ver":trim(fgets($f)));?></u>), was released on <u><?php echo $u?"Err reading ver":trim(fgets($f));?></u> and is <?php echo $u?"Err reading ver":trim(fgets($f));?>.</p></em>
 <br>
