40d8f67a904b081c23a5ca4820cf0cef3d3a9960
Zeale <Zeale@dusttoash.org>
2019-05-06 16:04:13 -0500

    Updated some styling and fixed a bug

--- dusttoash.org/testing/index.php
+++ dusttoash.org/testing/index.php
@@ -2,5 +2,5 @@
 <?php heading("Code Blocks"); ?>
 <div>A call to the
     <pre class="inline-code">printCode(resource)</pre>, given the CSS file that styles the code, produces the following:</div>
-<?php printCode(fopen("C:\wamp64\dusttoash.org\stylesheets\console.css", "r")); ?>
+<?php printCode(fopen("C:\wamp64\dusttoash\dusttoash.org\stylesheets\console.css", "r")); ?>
 <?php b(); ?>
