f92155923775e644df484c81600e1ff46bccd480
Zeale <zeale@dusttoash.org>
2019-08-06 22:59:30 -0500

    Removed some old files

--- stuff.dusttoash.org/index.php
+++
@@ -1,4 +0,0 @@
-<?php t(NULL,$header_links = array("<link rel=\"stylesheet\" type=\"text/css\" href=\"/boilerplate.css\">", "<link rel=\"stylesheet\" type=\"text/css\" href=\"index.css\">"));?>
-<h1>Stuff</h1>
-<p>Intoducing <b>Stuff</b>, a collection of general purpose, powerful tools for everyday life.</p>
-<p>This product is in it's early stages of development, but you're welcome to <a class=text-link href="/download">download</a> and try it out. By default, Stuff comes shipped with a calculator, (with support for expressions and massive numbers), a Task Organizer, Encryption/Decryption software, and more. Updates for the application are released as frequently as possible.</p><?php b();
