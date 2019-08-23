ebcfa733baf566776c5cadf3524be1b1da802caf
Zeale <Zeale@dusttoash.org>
2019-08-23 17:10:07 -0500

    Fix extraneous whitespace after version

--- stuff.dusttoash.org/download/changelog/index.php
+++ stuff.dusttoash.org/download/changelog/index.php
@@ -1,5 +1,5 @@
 <?php t("Changelog");$u=false;$f=fopen("../ver.txt", "r")or$u=true;?>
-<h1 style=font-size:4em;>Changelog<?php echo$u?"":" (".fgets($f).")"?></h1>
+<h1 style=font-size:4em;>Changelog<?php echo$u?"":" (".rtrim(fgets($f)).")"?></h1>
 <br>
 <p>
 Release Date: <b><?php echo fgets($f);?></b>
